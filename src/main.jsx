20e6045bb53980c2263d7aaed7215ecc539ccf0a
IBANZE Miracle <miraclecode11@gmail.com>
2026-03-23 20:37:03 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import AppContextProvider from "./features/AppContext.jsx";
 
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
